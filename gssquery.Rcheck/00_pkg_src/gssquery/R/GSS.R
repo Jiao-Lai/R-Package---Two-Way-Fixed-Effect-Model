dc1523903cdb2b3a786b385c0bb3846c9302ccf8
Jiao-Lai <78990586+Jiao-Lai@users.noreply.github.com>
2021-03-18 00:20:12 -0700

    Create GSS.R

---
+++ gssquery.Rcheck/00_pkg_src/gssquery/R/GSS.R
@@ -0,0 +1,14 @@
+#' Data on household happiness, income and other characteristics from `gss.norc.org`.
+#'
+#' This data is processed based on raw data downloaded from `gss.norc.org``, which can be found
+#' in folder 'data-raw'. Details of data preprocessing is included in `/data-raw/data_preprocessing.R`.
+#' The dta consists of a named list of variables including `happy` and `year`, which are the
+#' variables of interests. Other attributes such as `age`, `income` are related control variables.
+#'
+#' @name GSS
+#' @docType data
+#' @references [gss.norc.org
+#'     Data](http://gss.norc.org/get-the-data/stata)
+#' @keywords data
+#'
+NULL
